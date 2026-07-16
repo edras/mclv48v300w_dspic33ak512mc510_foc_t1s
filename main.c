517249298bbdb50acb9181adf165b0fb6d5edf4f
Edras Pacola <edras.pacola@microchip.com>
2026-07-16 13:15:13 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/**
+ * @file main.c
+ * @author M71906
+ * @date 2026-07-16
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+    while(1) {
+        
+    }
+
+    return 0;
+}
